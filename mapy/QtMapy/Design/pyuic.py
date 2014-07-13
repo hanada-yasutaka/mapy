93a2f0b9710e8b3588d4d4728128fab898b4fe79
Yasutaka Hanada <hanada1985@gmail.com>
2014-07-13 13:38:41 +0900

    change directory tree

---
+++ mapy/QtMapy/Design/pyuic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+if len(sys.argv) != 2:
+    raise TypeError("usage: %s sample.ui" % sys.argv[0])
+
+infile = sys.argv[1]
+outfile = infile.replace(".ui", ".py")
+os.system("pyuic4-3.3 %s > %s" % (infile, outfile))    
+os.system('gsed -i -e "s/QtGui.QMenuBar(MainWindow)/QtGui.QMenuBar() #QtGui.QStatusBar(MainWindow) /g" %s' % outfile)
+os.system('gsed -i -e "s/QtGui.QMenuBar(MainWindow)/QtGui.QMenuBar() #QtGui.QStatusBar(MainWindow) /g" %s' % outfile)
+os.system('gsed -i -e "s/from mplwidget import MplWidget/from .mplwidget import MplWidget/g" %s' % outfile)
